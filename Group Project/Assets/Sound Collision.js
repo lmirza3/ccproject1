dec1eb69a09932aba3624b405dd6a86fbb816b26
Goup4UIC <33033294+Goup4UIC@users.noreply.github.com>
2017-11-01 11:45:11 -0500

    scripts

---
+++ Group Project/Assets/Sound Collision.js
@@ -0,0 +1,21 @@
+﻿#pragma strict
+private var orange : Color = Color(0.8, 0.4, 0.0, 0.7);
+var newMaterial : Material;
+var target : Collider;
+private var counter : int = 0;
+var mySound : AudioClip;
+
+function OnTriggerEnter(cubeTrigger : Collider)
+{
+if (cubeTrigger == target)
+{
+
+
+GetComponent.<AudioSource>().PlayOneShot(mySound);
+counter = counter + 1;
+print("Collided: " + counter + " times!");
+}
+}
+
+
+
